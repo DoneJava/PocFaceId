42371e55d15a8ba09dbbe26c2bfc13ee649d77de
Jorge Sant Anna <jorge.santanna@cesgranrio.org.br>
2022-04-05 17:19:46 -0300

    Models criados

---
+++ PocFaceId/Migrations/20220405201754_Usuario.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PocFaceId.Migrations
+{
+    public partial class Usuario : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Foto",
+                table: "Pessoa",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Foto",
+                table: "Pessoa");
+        }
+    }
+}
--- PocFaceId/Model/Usuario.cs
+++ PocFaceId/Model/Usuario.cs
@@ -1,16 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace PocFaceId.Model
 {
-    public class Pessoa
+    public class Usuario
     {
         [Key]
         public int Id { get; set; }
-        public string Nome { get; set; }
-        public string Foto { get; set; }
+        public string Login { get; set; }
+        public string Senha { get; set; }
+        [ForeignKey("Pessoa")]
+        public int PessoaId { get; set; }
     }
 }
