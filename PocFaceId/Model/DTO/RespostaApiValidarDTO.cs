56578d96ea44492513d6161191eafc64123aea3c
ext_matheus <ext_matheus@cesgpon.onmicrosoft.com>
2022-04-26 15:24:49 -0300

    teste

---
+++ PocFaceId/Model/DTO/RespostaApiValidarDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PocFaceId.Model.DTO
+{
+    public class RespostaApiValidarDTO
+    {
+        public bool Validador { get; set; }
+        public string MensagemRsposta { get; set; }   
+    }
+}
